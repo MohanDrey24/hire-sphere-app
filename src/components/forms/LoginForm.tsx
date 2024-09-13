9bdbf75156035c41eca91dd5597899543b64cde9
Mohan Drey <mohantampon@gmail.com>
2024-09-13 18:43:01 +0800

    update login form

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -6,29 +6,44 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { Form } from '@/components/ui/form';
 import { Button } from '@/components/ui/button';
 import HFormField from '@/components/forms/HFormField';
+import { useQuery } from '@tanstack/react-query'
+import { useEffect } from 'react';
 
 const loginSchema = zod.object({
-  email: zod.string().email().min(1, { message: 'This field is required'}),
-  password: zod.string().min(1, { message: 'Password is required'})
+  email: zod
+    .string({ required_error: 'Email is required'})
+    .email({ message: 'Email should have valid format'}),
+  password: zod
+    .string({ required_error: 'Password is required'})
 })
 
 export default function LoginForm() {
+
   const form = useForm<zod.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
   });
 
   const { handleSubmit, control } = form
 
-  const onSubmit = (data: any) => {
-    console.log("HANDLE LOG IN LOGIC HERE")
+  const onSubmit = async(sample: any) => {
+    // TO DO
   }
 
+  const { data } = useQuery({ 
+    queryKey: ['yawa'], 
+    queryFn: () => {
+      return fetch('http://localhost:4000/api/jobs/all').then((res) => res.json())
+    }  
+  })
+
+  console.log("data na yawa", data)
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 space-y-6">
+    <div className="flex flex-col items-center justify-center min-h-screen space-y-6">
       <Form {...form}>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="lg:w-[1/4] sm:w-1/2 md:w-[500px] space-y-6"
+          className="lg:w-[1/4] md:w-[500px] sm:w-1/2 w-3/4 space-y-6"
         >
           <h1 className="text-2xl font-bold text-center">Welcome to Hire Sphere</h1>
 
@@ -55,7 +70,7 @@ export default function LoginForm() {
       </Form>
 
       <div 
-        className="relative lg:w-[1/4] md:w-[500px] sm:w-1/2 w-1/2 flex justify-center items-center"
+        className="relative lg:w-[1/4] md:w-[500px] sm:w-1/2 w-3/4 flex justify-center items-center"
       >
         <div className="flex-grow border-t border-black"></div>
         <span className="flex-shrink p-2">or</span>
@@ -64,7 +79,7 @@ export default function LoginForm() {
 
       <Button variant="outline">Continue with Google</Button>
   
-      <div >
+      <div>
         <span>Don't have an account?</span>
         <Button className="underline text-blue-600" variant="link">Sign up</Button>
       </div>
