c9f9336ae684f1cd707e364af7b9575ceb6cfae4
Mohan Drey <mohantampon@gmail.com>
2024-09-22 23:26:10 +0800

    add bg for login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import LoginForm from "@/components/forms/LoginForm";
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-green-100">
       <LoginForm />
-    </>
+    </div>
   );
 }
