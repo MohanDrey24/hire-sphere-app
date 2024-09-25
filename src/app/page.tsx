49b881f6ebff0709cb1f2947790ed9065589ae2d
Mohan Drey <mohantampon@gmail.com>
2024-09-25 18:20:24 +0800

    update index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import LoginForm from "@/components/forms/LoginForm";
 export default function Home() {
   return (
     <div className="flex flex-row">
-      <div className="flex w-[60%] h-screen bg-green-100 items-center justify-center">Placeholder image</div>
-      <LoginForm/>
+      <div className="flex w-0 md:w-0 lg:w-[60%] h-screen bg-green-100 items-center justify-center" />
+      <LoginForm />
     </div>
   );
 }
