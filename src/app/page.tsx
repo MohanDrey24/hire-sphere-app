19f95c2bfd73f61cc3e530576a7bf920ad2fdfd1
Mohan Drey <mohantampon@gmail.com>
2024-09-26 18:56:01 +0800

    update index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,18 @@ import LoginForm from "@/components/forms/LoginForm";
 
 export default function Home() {
   return (
-    <div className="flex flex-row">
-      <div className="flex w-0 md:w-0 lg:w-[60%] h-screen bg-green-100 items-center justify-center" />
-      <LoginForm />
+    <div className="h-screen flex flex-col">
+      <div className="h-16">navbar</div>
+      <div className="flex-1 flex flex-row">
+        <div className="hidden sm:hidden lg:block w-0 lg:w-1/2 xl:w-1/2 relative overflow-hidden">
+          <img 
+            className="absolute inset-0 w-full h-full object-cover"
+            src="/images/palette-ball.png"
+            alt="Palette ball"
+          />
+        </div>
+        <LoginForm className="w-full lg:w-1/2 flex flex-col items-center justify-center space-y-6"/>
+      </div>
     </div>
   );
 }
