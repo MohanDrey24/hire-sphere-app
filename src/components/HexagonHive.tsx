a58c3517dcb895c5d8fee07303ee5f952ad5085f
Mohan Drey <mohantampon@gmail.com>
2024-10-03 18:55:06 +0800

    update hexagon hive component

--- src/components/HexagonHive.tsx
+++ src/components/HexagonHive.tsx
@@ -2,28 +2,28 @@ import React from 'react';
 import Hexagon from './Hexagon';
 
 const HexagonHive: React.FC = () => {
-  // Function to create a column of hexagons
+
   const createColumn = (offset: boolean = false) => (
-    <div className={`flex flex-col space-y-1 ${offset ? '-translate-y-0' : ''}`}>
-      {[...Array(8)].map((_, index) => (
-        <Hexagon key={index} size="sm" color="blue" />
+    <div
+      className={`flex flex-col space-y-3 -translate-y-8 -translate-x-2 ${offset ? '-translate-y-4' : ''}`}
+    >
+      {[...Array(24)].map((_, index) => (
+        <Hexagon className="" key={index} size="sm" />
+        // <Hexagon className="-translate-x-8" key={index} size="sm" />
       ))}
     </div>
   );
 
-  // Function to create a set of two columns
   const createColumnSet = () => (
     <div className="flex">
       {createColumn()}
-      {createColumn(true)}
     </div>
   );
 
   return (
     <div className="flex h-screen overflow-hidden">
-      {/* We'll create 6 sets of columns to ensure full coverage */}
-      {[...Array(6)].map((_, index) => (
-        <div key={index} className={index % 2 === 0 ? '' : '-translate-y-0'}>
+      {[...Array(24)].map((_, index) => (
+        <div key={index} className={index % 2 === 0 ? '' : 'translate-y-[33px]'}>
           {createColumnSet()}
         </div>
       ))}
