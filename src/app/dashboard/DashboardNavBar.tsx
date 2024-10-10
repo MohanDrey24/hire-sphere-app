1729685b2f588c2c03c56a1ff2f10b8c53838876
Mohan Drey <mohantampon@gmail.com>
2024-10-10 17:55:54 +0800

    update dashboard nav bar

--- src/app/dashboard/DashboardNavBar.tsx
+++ src/app/dashboard/DashboardNavBar.tsx
@@ -1,27 +1,32 @@
+"use client"
 import Icon from "@/components/Icon";
 import { Input } from "@/components/ui/input";
+import { motion } from "framer-motion";
 
 export function DashboardNavBar () {
   return (
     <div className="flex justify-between bg-slate-100 h-20 min-w-full items-center">
       <div className="border-none ml-10 bg-red-100 w-10 h-10 rounded-full" />
 
-      {/* Icon should be an optional prop for Input component */}
       <div className="flex flex-row relative">
         <Input
           className="w-[300px]"
           placeholder="Search for jobs"
-        />
-        <Icon
-          className="absolute inset-y-0 right-[15px] top-[15px] cursor-pointer"
-          alt="search"
-          src="/icons/search.svg"
-          height="20px"
-          width="20px"
-        />
+        >
+          <Icon
+            alt="search"
+            src="/icons/search.svg"
+            height="20px"
+            width="20px"
+          />
+        </Input>
       </div>
 
-      <div className="">Logo dropdown</div>
+      <motion.div 
+        className="mr-8 rounded-full w-10 h-10 bg-blue-600 cursor-pointer border-2"
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.8 }}
+      />
     </div>
   );
 }
