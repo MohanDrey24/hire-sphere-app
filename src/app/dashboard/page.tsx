04903a87d438f0a3d604b51b5bf8d9edc086b5ec
Mohan Drey <mohantampon@gmail.com>
2024-10-13 21:42:55 +0800

    add notes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { DashboardNavBar } from "./DashboardNavBar"
 export default function Dashboard() {
   return(
     <div>
+      {/* MOVE NAVBAR TO LAYOUT */}
       <DashboardNavBar />
     </div>
   );
