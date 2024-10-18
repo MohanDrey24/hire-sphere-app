130681a3435b79608cd8791c8523dc05901e6c4d
Mohan Drey <mohantampon@gmail.com>
2024-10-18 17:51:47 +0800

    update dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,17 @@
-import { DashboardNavBar } from "./DashboardNavBar"
+"use client"
+
+import { DashboardNavBar } from "./DashboardNavBar";
+import { useSession } from "next-auth/react"
 
 export default function Dashboard() {
+  const { data: session, status } = useSession()
+
 
   return (
     <div>
       {/* SHOULD BE IN A LAYOUT */}
       <DashboardNavBar />
+      <div>session: {status}</div>
     </div>
   );
 }
