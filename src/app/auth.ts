9d30a6575fe7655ca1c8ba2bfd80980c4cd1931c
Mohan Drey <mohantampon@gmail.com>
2024-10-18 17:51:22 +0800

    update auth file

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,6 +1,46 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
- 
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google,
+    Credentials({
+      credentials: {
+        email: {},
+        password: {}
+      },
+      async authorize(credentials) {
+        const res = await fetch("http://localhost:4000/api/auth/signin", {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          credentials: 'include',
+          headers: { "Content-Type": "application/json" }
+        })
+
+        if (res.ok) {
+          const user = await res.json()
+          return user
+        }
+
+        return null
+      },
+    })
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+        token.name = user.name
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string
+        session.user.name = token.name as string
+      }
+      return session
+    }
+  }
 })
