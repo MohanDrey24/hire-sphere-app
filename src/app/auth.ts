24f520294fbe8d8852b06a176f5d3a33e349e409
Mohan Drey <mohantampon@gmail.com>
2024-10-22 18:04:21 +0800

    comment for debugging purposes

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -10,37 +10,37 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         email: {},
         password: {}
       },
-      async authorize(credentials) {
-        const res = await fetch("http://localhost:4000/api/auth/signin", {
-          method: 'POST',
-          body: JSON.stringify(credentials),
-          credentials: 'include',
-          headers: { "Content-Type": "application/json" }
-        })
+      // async authorize(credentials) {
+      //   const res = await fetch("http://localhost:4000/api/auth/signin", {
+      //     method: 'POST',
+      //     body: JSON.stringify(credentials),
+      //     credentials: 'include',
+      //     headers: { "Content-Type": "application/json" }
+      //   })
 
-        if (res.ok) {
-          const user = await res.json()
-          return user
-        }
+      //   if (res.ok) {
+      //     const user = await res.json()
+      //     return user
+      //   }
 
-        return null
-      },
+      //   return null
+      // },
     })
   ],
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id
-        token.name = user.name
-      }
-      return token
-    },
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.id as string
-        session.user.name = token.name as string
-      }
-      return session
-    }
-  }
+  // callbacks: {
+    // async jwt({ token, user }) {
+    //   if (user) {
+    //     token.id = user.id
+    //     token.name = user.name
+    //   }
+    //   return token
+    // },
+    // async session({ session, token }) {
+    //   if (session.user) {
+    //     session.user.id = token.id as string
+    //     session.user.name = token.name as string
+    //   }
+    //   return session
+    // }
+  // }
 })
