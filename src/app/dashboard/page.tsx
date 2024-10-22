cbfd176d449ddfe0f34e075ac2c8b1ef6471ed6b
Mohan Drey <mohantampon@gmail.com>
2024-10-22 18:04:36 +0800

    remove useSession

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,10 @@
-"use client"
-
 import { DashboardNavBar } from "./DashboardNavBar";
-import { useSession } from "next-auth/react"
 
 export default function Dashboard() {
-  const { data: session, status } = useSession()
-
-
   return (
     <div>
       {/* SHOULD BE IN A LAYOUT */}
       <DashboardNavBar />
-      <div>session: {status}</div>
     </div>
   );
 }
