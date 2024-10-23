d8591c86acdd3191fe67bdab054f4fed24ef8e6c
Mohan Drey <mohantampon@gmail.com>
2024-10-23 17:19:13 +0800

    add dashboard main component

---
+++ src/app/dashboard/DashboardMain.tsx
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { type Job } from "./types";
+import JobCard from "./JobCard";
+
+export default function DashboardMain () {
+  const { isPending, data, error } = useQuery<Array<Job>>({
+    queryKey: ['jobs'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:4000/api/jobs/all', {
+        credentials: 'include'
+      })
+
+      return await response.json()
+    }
+  })
+
+  return (
+    <JobCard jobData={data} />
+  );
+};
