5650b794bff5c5f23580a3bbe528797d62aee2a7
Mohan Drey <mohantampon@gmail.com>
2024-11-11 17:44:05 +0800

    update dashboard main

--- src/app/dashboard/DashboardMain.tsx
+++ src/app/dashboard/DashboardMain.tsx
@@ -1,37 +1,26 @@
 "use client"
 
 import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 import JobCard from "./JobCard";
 import useJobStore from "../stores/useJobStore";
-import { useQuery } from "@tanstack/react-query";
-import  type { Job, User } from "./types";
 import useFetch from "@/hooks/useFetch"
-import useUserStore from "../stores/useUserStore";
+import type { Job } from "./types";
 
 export default function DashboardMain () {
   const setJobs = useJobStore((state) => state.setJobs);
   const jobState = useJobStore((state) => state.jobs);
   
-  const setUser = useUserStore((state) => state.setUser);
-
-  const { isPending, data: jobsData, isSuccess: jobsSuccess } = useQuery<Array<Job>>({
+  const { isPending, data: jobsData, isSuccess: jobsSuccess } = useQuery<Job[]>({
     queryKey: ['jobs'],
     queryFn: () => useFetch('/jobs/all')
-  })
-
-  const { data: userData, isSuccess: userDataSuccess} = useQuery<User>({
-    queryKey: ['users'],
-    queryFn: () => useFetch('/users/current')
   })
 
   useEffect(() => {
     if (jobsSuccess) {
       setJobs(jobsData);
     }
-    if (userDataSuccess) {
-      setUser(userData);
-    }
-  }, [jobsSuccess, jobsData, userDataSuccess, userData, setJobs, setUser]);
+  }, [jobsSuccess, jobsData, setJobs]);
 
   return (
     <>
