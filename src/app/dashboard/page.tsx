e8fe553ef3477808f086364f02b617a256bc6760
Mohan Drey <mohantampon@gmail.com>
2024-11-18 18:24:40 +0800

    add grid layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,14 @@
 import DashboardMain from "./DashboardMain";
 import { DashboardNavBar } from "./DashboardNavBar";
+import { JobDetails } from "./JobDetails";
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="grid grid-cols-8 grid-rows-8 h-screen w-screen">
       {/* SHOULD BE IN A LAYOUT */}
-      <DashboardNavBar />
-      <DashboardMain />
+      <DashboardNavBar className="col-span-8"/>
+      <DashboardMain className="col-span-3 row-span-7 row-start-2"/>
+      <JobDetails className="col-span-5 row-span-7 col-start-4 row-start-2"/>
     </div>
   );
 }
