9ec317fcad80ffa456aed8e22c2ae4c28e360cb3
Mohan Drey <mohantampon@gmail.com>
2024-11-18 18:24:30 +0800

    add job details component

---
+++ src/app/dashboard/JobDetails.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  className?: string;
+}
+
+export function JobDetails ({ className }: Props) {
+ return (
+  <div className={className}>
+    <p className="text-[100px]">SAMPLE</p>
+  </div>
+ )
+}
