4f2c5f6c93de168c2b2c2e78a548c8a61f3f2a22
Mohan Drey <mohantampon@gmail.com>
2024-11-19 15:47:32 +0800

    update job details bg

--- src/app/dashboard/JobDetails.tsx
+++ src/app/dashboard/JobDetails.tsx
@@ -5,7 +5,7 @@ type Props = {
 export function JobDetails ({ className }: Props) {
  return (
   <div className={className}>
-    <p className="text-[100px]">SAMPLE</p>
+    <p className="bg-green-100 w-full h-full text-[100px] flex items-center justify-center">SAMPLE</p>
   </div>
  )
 }
