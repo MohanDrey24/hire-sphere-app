68e894ea155cfca28246f033a9c90d0ee93fcbb5
Mohan Drey <mohantampon@gmail.com>
2024-11-19 15:47:44 +0800

    add responsive design to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,8 +7,8 @@ export default function Dashboard() {
     <div className="grid grid-cols-8 grid-rows-8 h-screen w-screen">
       {/* SHOULD BE IN A LAYOUT */}
       <DashboardNavBar className="col-span-8"/>
-      <DashboardMain className="col-span-3 row-span-7 row-start-2"/>
-      <JobDetails className="col-span-5 row-span-7 col-start-4 row-start-2"/>
+      <DashboardMain className="overflow-y-auto col-span-8 row-span-7 sm:col-span-3 sm:row-span-7 sm:row-start-2"/>
+      <JobDetails className="hidden sm:block col-span-5 row-span-7 col-start-4 row-start-2"/>
     </div>
   );
 }
