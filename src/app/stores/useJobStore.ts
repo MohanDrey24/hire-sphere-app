6afca8be34b6edaafa2d9e7603c047f9598211ef
Mohan Drey <mohantampon@gmail.com>
2024-11-20 18:34:02 +0800

    add selected job id state

--- src/app/stores/useJobStore.ts
+++ src/app/stores/useJobStore.ts
@@ -3,15 +3,19 @@ import type { Job } from '../dashboard/types';
 
 type JobState = {
   jobs: Job[];
+  selectedJobId: string;
 };
 
 type JobAction = {
   setJobs: (job: Job[]) => void;
+  setSelectedJobId: (id: string) => void;
 };
 
 const useJobStore = create<JobState & JobAction>((set) => ({
   jobs: [],
-  setJobs: (jobs: Job[]) => set({ jobs })
+  selectedJobId: "",
+  setJobs: (jobs: Job[]) => set({ jobs }),
+  setSelectedJobId: (id: string) => set({ selectedJobId: id })
 }))
 
 export default useJobStore;
