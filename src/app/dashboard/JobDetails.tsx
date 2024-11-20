88be7cdbedfa30aa4a86c72dc15fde82d8033d80
Mohan Drey <mohantampon@gmail.com>
2024-11-20 18:34:36 +0800

    fetch job description based on selected job id

--- src/app/dashboard/JobDetails.tsx
+++ src/app/dashboard/JobDetails.tsx
@@ -1,11 +1,24 @@
+"use client";
+import useJobStore from "../stores/useJobStore";
+import { Job } from "./types";
+
 type Props = {
   className?: string;
 }
 
 export function JobDetails ({ className }: Props) {
+  const selectedJobId: string = useJobStore((state) => state.selectedJobId);
+  const jobState: Job[] = useJobStore((state) => state.jobs)
+
+  const selectedJob = jobState.find((job) => job.id === selectedJobId);
+
  return (
-  <div className={className}>
-    <p className="bg-green-100 w-full h-full text-[100px] flex items-center justify-center">SAMPLE</p>
+  <div className={`${className} bg-green-100 w-full h-full flex items-center justify-center`} >
+    {selectedJob?.description ? (
+      <p className="text-md">{selectedJob.description}</p>
+    ) : (
+      <p className="text-md">No Job Description</p>
+    )}
   </div>
  )
 }
