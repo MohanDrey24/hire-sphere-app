206dd1efda65a2e19b80aeb25aa373714c6298bd
Mohan Drey <mohantampon@gmail.com>
2024-11-21 17:40:44 +0800

    update dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,14 @@
-import DashboardMain from "./DashboardMain";
-import { DashboardNavBar } from "./DashboardNavBar";
+import Main from "./Main";
+import { NavBar } from "./NavBar";
 import { JobDetails } from "./JobDetails";
 
 export default function Dashboard() {
   return (
     <div className="grid grid-cols-8 grid-rows-8 h-screen w-screen">
       {/* SHOULD BE IN A LAYOUT */}
-      <DashboardNavBar className="col-span-8"/>
-      <DashboardMain className="overflow-y-auto col-span-8 row-span-7 sm:col-span-3 sm:row-span-7 sm:row-start-2"/>
-      <JobDetails className="hidden sm:block col-span-5 row-span-7 col-start-4 row-start-2"/>
+      <NavBar className="col-span-8"/>
+      <Main className="overflow-y-auto col-span-8 row-span-7 sm:col-span-3 sm:row-span-7 sm:row-start-2"/>
+      <JobDetails className="overflow-y-auto hidden sm:block col-span-5 row-span-7 col-start-4 row-start-2"/>
     </div>
   );
 }
