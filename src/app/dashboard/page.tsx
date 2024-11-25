27e30cc6a2422c154f51bf7e08c07ac1d7a50a6c
Mohan Drey <mohantampon@gmail.com>
2024-11-25 18:07:26 +0800

    update responsiveness of dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,34 @@
+"use client";
+
 import Main from "./Main";
 import { NavBar } from "./NavBar";
 import { JobDetails } from "./JobDetails";
+import useJobStore from "../stores/useJobStore";
 
 export default function Dashboard() {
+  const selectedJobId = useJobStore((state) => state.selectedJobId);
+
   return (
     <div className="grid grid-cols-8 grid-rows-8 h-screen w-screen">
-      {/* SHOULD BE IN A LAYOUT */}
-      <NavBar className="col-span-8"/>
-      <Main className="overflow-y-auto col-span-8 row-span-7 sm:col-span-3 sm:row-span-7 sm:row-start-2"/>
-      <JobDetails className="overflow-y-auto hidden sm:block col-span-5 row-span-7 col-start-4 row-start-2"/>
+
+      <NavBar className="col-span-8" />
+
+      <Main 
+        className={`
+          overflow-y-auto col-span-8 row-span-7
+          sm:col-span-3 sm:row-span-7 sm:row-start-2
+          ${selectedJobId ? "hidden sm:block" : ""}
+        `}
+      />
+
+      <JobDetails 
+        className={`
+          overflow-y-auto col-span-8 row-span-7 row-start-2
+          sm:col-span-5 sm:row-span-7 sm:col-start-4
+          hidden sm:block
+        `}
+      />
+
     </div>
   );
 }
