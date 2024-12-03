dcc854aea5f57e2a5302dc3b2c33f25cecad2db6
Mohan Drey <mohantampon@gmail.com>
2024-12-03 18:15:39 +0800

    add styling when search bar is clicked

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 export default function SearchBar() {
   return (
-    <div className="flex border border-black rounded-md transition-colors focus-visible:border-blue-100 focus:border-blue-100 focus:border-1 shadow-sm">
+    <div className="flex border rounded-md focus-within:border-blue-500 focus-within:border-2">
 
       <Input
         className="w-44 sm:w-80 pr-0 border-0 shadow-none focus-visible:ring-0 focus-visible:ring-none"
