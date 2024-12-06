e623447cfb74c881e0c3626430f68f44b347e779
Mohan Drey <mohantampon@gmail.com>
2024-12-06 16:43:55 +0800

    update search bar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,14 +1,21 @@
+"use client";
+
+import { useState } from "react";
 import Icon from "./Icon";
 import { Input } from "./ui/input";
 import { motion } from "framer-motion";
 
 export default function SearchBar() {
+  const [searchInput, setSearchInput] = useState("");
+
   return (
     <div className="flex border rounded-md focus-within:border-blue-500 focus-within:border-2">
 
       <Input
         className="w-44 sm:w-80 pr-0 border-0 shadow-none focus-visible:ring-0 focus-visible:ring-none"
         placeholder="Search for jobs"
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
       />
 
       <motion.button
