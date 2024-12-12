abdd76f3d11aa7840da07de447b7b67b7763fc70
Mohan Drey <mohantampon@gmail.com>
2024-12-12 18:23:45 +0800

    update search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,36 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Icon from "./Icon";
 import { Input } from "./ui/input";
 import { motion } from "framer-motion";
+import { useQuery } from "@tanstack/react-query";
+import useFetch from "@/hooks/useFetch";
+import { debounce } from "lodash";
 
 export default function SearchBar() {
   const [searchInput, setSearchInput] = useState("");
+
+  const searchAutocomplete = (params: string): any => {
+    // const { isPending, data, isSuccess } = useQuery<any>({
+    //   queryKey: ["search", params],
+    //   queryFn: () => useFetch(`/jobs/autocomplete?position=${params}`)
+    // });
+
+    // return { isPending, data, isSuccess, isLoading, isError };
+  };
+
+  const debouncedSearch = useCallback(
+    debounce((query: string) => {
+      setSearchInput(query);
+      searchAutocomplete(query);
+    }, 300),
+    []
+  );
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    debouncedSearch(e.target.value)
+  }
 
   return (
     <div className="flex border rounded-md focus-within:border-blue-500 focus-within:border-2">
@@ -15,7 +39,7 @@ export default function SearchBar() {
         className="w-44 sm:w-80 pr-0 border-0 shadow-none focus-visible:ring-0 focus-visible:ring-none"
         placeholder="Search for jobs"
         value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
+        onChange={handleInputChange}
       />
 
       <motion.button
@@ -30,7 +54,6 @@ export default function SearchBar() {
           width="20px"
         />
       </motion.button>
-
     </div>
   );
 }
