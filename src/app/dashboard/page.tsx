fe5a00944b669b90122773056bca48c346af4fdf
Mohan Drey <mohantampon@gmail.com>
2024-12-23 17:38:10 +0800

    remove unnecessary fetch in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,31 +1,15 @@
 "use client";
 
-import { useEffect } from "react";
 import { NavBar } from "./NavBar";
 import { JobDetails } from "./JobDetails";
 import JobCard from "./JobCard";
 import useJobStore from "../stores/useJobStore";
-import { useQuery } from "@tanstack/react-query";
-import type { Job } from "./types";
-import useFetch from "@/hooks/useFetch";
 
 export default function Dashboard() {
   const selectedJobId = useJobStore((state) => state.selectedJobId);
 
-  const setJobs = useJobStore((state) => state.setJobs);
   const jobState = useJobStore((state) => state.jobs);
   
-  const { isPending, data: jobsData, isSuccess: jobsSuccess } = useQuery<Job[]>({
-    queryKey: ["jobs"],
-    queryFn: () => useFetch("/jobs/all")
-  })
-
-  useEffect(() => {
-    if (jobsSuccess) {
-      setJobs(jobsData);
-    }
-  }, [jobsSuccess, jobsData, setJobs]);
-
   return (
     <div className="grid grid-cols-8 grid-rows-8 h-screen w-screen">
       <NavBar className="col-span-8" />
@@ -38,7 +22,7 @@ export default function Dashboard() {
           sm:block sm:col-span-3
         `}
         jobData={jobState}
-        isPending={isPending}
+        isPending={false}
       />
 
       <JobDetails
