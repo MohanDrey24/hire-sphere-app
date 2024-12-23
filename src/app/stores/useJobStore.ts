6f7897500272d90105dd4278dea7c297c804dd96
Mohan Drey <mohantampon@gmail.com>
2024-12-23 17:37:14 +0800

    update job store

--- src/app/stores/useJobStore.ts
+++ src/app/stores/useJobStore.ts
@@ -4,18 +4,22 @@ import type { Job } from '../dashboard/types';
 type JobState = {
   jobs: Job[];
   selectedJobId: string;
+  showAutocomplete: boolean;
 };
 
 type JobAction = {
   setJobs: (job: Job[]) => void;
   setSelectedJobId: (id: string) => void;
+  setShowAutocomplete: (condition: boolean) => void;
 };
 
 const useJobStore = create<JobState & JobAction>((set) => ({
   jobs: [],
   selectedJobId: "",
+  showAutocomplete: true,
   setJobs: (jobs: Job[]) => set({ jobs }),
-  setSelectedJobId: (id: string) => set({ selectedJobId: id })
+  setSelectedJobId: (id: string) => set({ selectedJobId: id }),
+  setShowAutocomplete: (condition) => set({ showAutocomplete: condition }),
 }))
 
 export default useJobStore;
