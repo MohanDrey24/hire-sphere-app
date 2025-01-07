f24ee8ae2ef5780d9556bb56fa95fe7a5570d74f
Mohan Drey <mohantampon@gmail.com>
2025-01-07 17:16:26 +0800

    refactor job details component

--- src/app/dashboard/JobDetails.tsx
+++ src/app/dashboard/JobDetails.tsx
@@ -2,6 +2,7 @@
 
 import useJobStore from "../stores/useJobStore";
 import purify from "dompurify";
+import { useSearchParams } from "next/navigation";
 
 type JobDetailsProp = {
   className?: string;
@@ -9,20 +10,33 @@ type JobDetailsProp = {
 }
 
 export function JobDetails ({ className, selectedJobId }: JobDetailsProp) {
+  const searchParams = useSearchParams();
   const jobState = useJobStore((state) => state.jobs);
   const selectedJob = jobState.find((job) => job.id === selectedJobId);
 
- return (
-  <div className={`${className} w-full h-full`}>
-    <div className="w-full h-full overflow-y-auto p-4 pt-6">
-      {selectedJob?.description ? (
-        <div dangerouslySetInnerHTML={{ __html: purify.sanitize(selectedJob?.description) }} />
-      ) : (
-        <div className="h-full w-full flex items-center justify-center">
-          <p>No Job Description</p>
-        </div>
-      )}
+  const renderContent = () => {
+    if (selectedJob?.description) {
+      return <div dangerouslySetInnerHTML={{ 
+        __html: purify.sanitize(selectedJob.description)
+      }} />;
+    }
+
+    const message = searchParams.has("job-id")
+      ? "No Job Description"
+      : "Select a Job";
+
+    return (
+      <div className="h-full w-full flex items-center justify-center">
+        <p>{message}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div className={`${className} w-full h-full`}>
+      <div className="w-full h-full overflow-y-auto p-4 pt-6">
+        {renderContent()}
+      </div>
     </div>
-  </div>
- )
+  )
 };
