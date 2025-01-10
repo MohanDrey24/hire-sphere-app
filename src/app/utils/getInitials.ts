a6e0bd3677b9761508fb7aae939cd9823b0718db
Mohan Drey <mohantampon@gmail.com>
2025-01-10 16:41:02 +0800

    create get initials util function

---
+++ src/app/utils/getInitials.ts
@@ -0,0 +1,19 @@
+import { type User } from "../dashboard/types";
+
+export const getInitials = (user: User | null): string => {
+  if (!user) return '';
+
+  if (user.name?.trim()) {
+    return user.name
+      .trim()
+      .split(' ')
+      .filter(Boolean)
+      .map(word => word[0]?.toUpperCase())
+      .join('')
+  }
+  
+  return (
+    (user.firstName?.[0] || '') +
+    (user.lastName?.[0] || '')
+  ).toUpperCase();
+}
