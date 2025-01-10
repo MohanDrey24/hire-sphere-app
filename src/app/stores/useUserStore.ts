433568653859791b3a8a843875ff6ce4897722a5
Mohan Drey <mohantampon@gmail.com>
2025-01-10 16:40:25 +0800

    update user store

--- src/app/stores/useUserStore.ts
+++ src/app/stores/useUserStore.ts
@@ -1,17 +1,28 @@
 import { create } from 'zustand';
 import { type User } from '../dashboard/types';
+import useFetch from '@/hooks/useFetch';
+import { useQuery } from '@tanstack/react-query';
 
 type UserState = {
   user: User | null
 }
 
 type UserAction = {
-  setUser: (user: User) => void
+  setUser: () => void
 }
 
 const useUserStore = create<UserState & UserAction>((set) => ({
   user: null,
-  setUser: (user: User) => set({ user })
+  setUser: () => {
+    const { data } = useQuery<User>({
+      queryKey: ['users'],
+      queryFn: () => useFetch('/users/current')
+    })
+
+    if (data) {
+      set({ user: data })
+    }
+  }
 }))
 
 export default useUserStore;
