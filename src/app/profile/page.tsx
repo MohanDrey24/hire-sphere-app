37c70f00b9ac1027e62722b700630576f52f0f87
Mohan Drey <mohantampon@gmail.com>
2025-01-23 16:51:19 +0800

    update the UI of profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import useUserStore from "../stores/useUserStore"
+import useUserStore from "../stores/useUserStore";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { getInitials } from "../utils/getInitials";
 import { formatDate } from "../utils/formatDate";
@@ -9,46 +9,57 @@ import { UserPenIcon, SquareXIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import ProfileField from "./ProfileField";
 
-export default function Profile () {
+export default function Profile() {
   const [isEditing, setEditingMode] = useState(false);
-  const [editedProfile, setEditedProfile] = useState(null);
-
   const userState = useUserStore((state) => state.user);
   const setUser = useUserStore((state) => state.setUser);
-  
-  setUser()
+
+  setUser();
 
   return (
-    <div>
-      <p className="text-4xl">PROFILE:</p>
-      <div className="flex items-center gap-20">
-        <Avatar className="w-40 h-40">
-          <AvatarImage
-            alt="Avatar"
-            src={userState?.image}
-          />
-          <AvatarFallback className="text-white font-bold text-5xl">
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-10 p-5 sm:p-20">
+      <p className="items-center text-4xl">PROFILE</p>
+      <div className="flex w-3/4 flex-col items-center gap-10 sm:w-3/4 md:flex-row">
+        <Avatar className="h-40 w-40">
+          <AvatarImage alt="Avatar" src={userState?.image} />
+          <AvatarFallback className="text-5xl font-bold text-white">
             {getInitials(userState)}
           </AvatarFallback>
         </Avatar>
 
-        <div className="flex flex-col gap-2">
-          <ProfileField 
+        <div className="flex w-full flex-col gap-2">
+          {!isEditing && (
+            <div className="self-end">
+              <Button
+                variant="ghost"
+                size="icon"
+                aria-label="Edit Profile"
+                onClick={() => setEditingMode(!isEditing)}
+              >
+                <UserPenIcon />
+              </Button>
+            </div>
+          )}
+
+          <ProfileField
+            className="pb-4"
             label="Display Name"
             name="Display Name"
             isEditing={isEditing}
             value={userState?.name}
           />
 
-          <div className="flex gap-2">
-            <ProfileField 
+          <div className="flex flex-col gap-4 sm:flex-row">
+            <ProfileField
+              className="w-full sm:w-1/2"
               label="First Name"
               name="First Name"
               isEditing={isEditing}
               value={userState?.firstName}
             />
 
-            <ProfileField 
+            <ProfileField
+              className="w-full sm:w-1/2"
               label="Last Name"
               name="Last Name"
               isEditing={isEditing}
@@ -58,40 +69,28 @@ export default function Profile () {
         </div>
       </div>
 
-      { !isEditing && 
-        <Button
-          variant="ghost"
-          size="icon"
-          aria-label="Edit Profile"
-          onClick={() => setEditingMode(!isEditing)}
-        >
-          <UserPenIcon />
-        </Button>
-      }
-
       <div>
-        <ul className="space-y-2">
+        <ul className="flex flex-col space-y-2 sm:space-y-0 md:flex-row">
           <li className="flex items-center gap-2">
             <span className="text-gray-700">Email:</span>
-            { !isEditing 
-              ? <p className="font-bold">{userState?.email}</p> 
-              : <p>Placeholder</p>
-            }
+            <p className="font-bold">{userState?.email}</p>
           </li>
           <li className="flex items-center gap-2">
             <span className="text-gray-700">Joined at:</span>
-            <p className="font-bold">{formatDate(userState?.createdAt, "MM/DD/YYYY")}</p>
+            <p className="font-bold">
+              {formatDate(userState?.createdAt, "MM/DD/YYYY")}
+            </p>
           </li>
         </ul>
       </div>
 
-      { isEditing && (
+      {isEditing && (
         <div className="flex gap-4">
           <Button type="submit">Save Changes</Button>
           <Button
             variant="ghost"
             size="icon"
-            type="button" 
+            type="button"
             onClick={() => setEditingMode(false)}
           >
             <SquareXIcon />
@@ -99,5 +98,5 @@ export default function Profile () {
         </div>
       )}
     </div>
-  )
-};
+  );
+}
