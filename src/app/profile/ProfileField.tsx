4afde52c1d2615dffe58ebb50befdaf85530a710
Mohan Drey <mohantampon@gmail.com>
2025-01-23 16:50:37 +0800

    update styling and formatting of profilefield component

--- src/app/profile/ProfileField.tsx
+++ src/app/profile/ProfileField.tsx
@@ -1,18 +1,26 @@
 import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
 
 interface ProfileFieldProps {
   value?: string;
   label: string;
   isEditing: boolean;
   name: string;
+  className?: string;
 }
 
-export default function ProfileField ({ value, label, isEditing, name }: ProfileFieldProps) {
+export default function ProfileField({
+  value,
+  label,
+  isEditing,
+  name,
+  className,
+}: ProfileFieldProps) {
   return (
-    <div className="*:text-2xl gap-2">
-      <span className="text-gray-700">{label}:</span>
+    <div className={cn("flex w-full flex-col gap-1", className)}>
+      <span className="text-md text-gray-700">{label}</span>
       <Input
-        className="w-full"
+        className="w-full focus-visible:ring-2 focus-visible:ring-blue-300 focus-visible:ring-offset-1"
         name={name}
         disabled={!isEditing}
         value={value}
