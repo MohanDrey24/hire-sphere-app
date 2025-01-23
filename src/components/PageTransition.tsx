1d01677396782246c097dfe09fb38f6d2b3985c4
Mohan Drey <mohantampon@gmail.com>
2025-01-23 16:50:27 +0800

    update formatting

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -3,34 +3,34 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
-const PageTransition = ({ 
-  children 
-} : Readonly<{ 
-  children: React.ReactNode
-}> ) => {
+const PageTransition = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
   const pathname = usePathname();
   return (
     <AnimatePresence>
       <motion.div
         key={pathname}
         initial={{ opacity: 0 }}
-        animate={{ 
+        animate={{
           opacity: 1,
-          transition: { duration: 0.4, ease: "easeInOut" }
+          transition: { duration: 0.4, ease: "easeInOut" },
         }}
       >
         <motion.div
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
-            transition: { delay: 0.1, duration: 0.4, ease: "easeInOut"}
+            transition: { delay: 0.1, duration: 0.4, ease: "easeInOut" },
           }}
-          className="fixed h-screen w-screen top-0 pointer-events-none"
+          className="pointer-events-none fixed top-0 h-screen w-screen"
         />
         {children}
       </motion.div>
     </AnimatePresence>
-  )
-}
+  );
+};
 
 export default PageTransition;
