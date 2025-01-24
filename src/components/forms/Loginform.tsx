70b7e6a782f110301b0f9e2b8f96498fc0ac7cfb
Mohan Drey <mohantampon@gmail.com>
2025-01-24 17:42:00 +0800

    update login form

--- src/components/forms/Loginform.tsx
+++ src/components/forms/Loginform.tsx
@@ -1,84 +1,71 @@
-"use client"
+"use client";
 
-import * as zod from 'zod';
-import { memo } from 'react';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Form } from '@/components/ui/form';
-import { Button } from '@/components/ui/button';
-import HFormField from './HFormField';
-import { useRouter } from 'next/navigation';
-import { useMutationAPI } from '@/hooks/useMutationAPI';
-import Icon from '../Icon';
-import { MouseEventHandler } from 'react';
-
-const loginSchema = zod.object({
-  email: zod
-    .string()
-    .email({ message: 'Email should have valid format'}),
-  password: zod
-    .string({ required_error: 'Password is required'})
-    .min(1, { message: 'Password is required'})
-})
-
-type LoginFormData = zod.infer<typeof loginSchema>
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import HFormField from "./HFormField";
+import { useRouter } from "next/navigation";
+import Icon from "../Icon";
+import { MouseEventHandler } from "react";
+import { loginSchema, LoginFormData, useLogin } from "@/lib/auth";
 
 type LoginFormProps = {
   className?: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
-}
+};
 
 function LoginForm({ className, onClick }: LoginFormProps) {
+  const router = useRouter();
+
+  const { status: signInStatus, mutate } = useLogin();
+  const status = signInStatus === "pending";
+
   const form = useForm<LoginFormData>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
-      email: '',
-      password: ''
+      email: "",
+      password: "",
     },
-    mode: 'onChange',
-    shouldUseNativeValidation: false
+    mode: "onChange",
+    shouldUseNativeValidation: false,
   });
 
-  const { 
-    handleSubmit, 
+  const {
+    handleSubmit,
     control,
     setError,
-    formState: { errors } 
-  } = form
-
-  const router = useRouter()
-
-  const { status: signInStatus, mutate: signIn } = useMutationAPI("/auth/signin")
-
-  const status = signInStatus === 'pending'
+    formState: { errors },
+  } = form;
 
   const onSubmit = (value: LoginFormData) => {
-    setError('root', { message: undefined })
-    signIn(value, {
+    setError("root", { message: undefined });
+    mutate(value, {
       onSuccess: () => {
-        router.push("/dashboard")
+        router.push("/dashboard");
       },
       onError: () => {
         setError("root", {
-          message: "Invalid Credentials"
-        })
-      }
-    })
-  }
+          message: "Invalid Credentials",
+        });
+      },
+    });
+  };
 
   const navigateToGoogleRedirectURI = () => {
-    router.push(process.env.NEXT_PUBLIC_GOOGLE_OAUTH2_CALLBACK_URL || '')
-  }
+    router.push(process.env.NEXT_PUBLIC_GOOGLE_OAUTH2_CALLBACK_URL || "");
+  };
 
   return (
     <div className={className}>
       <Form {...form}>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="lg:w-3/4 sm:max-md:w-[60%] w-[60%] space-y-6"
+          className="w-[60%] space-y-6 sm:max-md:w-[60%] lg:w-3/4"
         >
-
-          <h1 className="text-2xl font-bold text-center">Welcome to Hire Sphere</h1>
+          <h1 className="text-center text-2xl font-bold">
+            Welcome to Hire Sphere
+          </h1>
 
           <HFormField
             control={control}
@@ -87,54 +74,52 @@ function LoginForm({ className, onClick }: LoginFormProps) {
             type="email"
             placeholder="Enter your email here"
             disabled={status}
-            onClick={() => setError('root', { message: undefined })}
+            onClick={() => setError("root", { message: undefined })}
           />
 
           <HFormField
             control={control}
-            label="Password" 
+            label="Password"
             name="password"
             type="password"
             placeholder="Enter your password here"
             disabled={status}
-            onClick={() => setError('root', { message: undefined })}
+            onClick={() => setError("root", { message: undefined })}
           />
 
           <Button
             type="submit"
             variant="default"
-            className="w-full h-12"
+            className="h-12 w-full"
             disabled={status}
           >
-            { status ? 'Loading...' :'Log in' }
+            {status ? "Loading..." : "Log in"}
           </Button>
         </form>
       </Form>
 
       {errors.root?.message && (
-        <div className="text-red-500 text-center">
-          {errors.root.message}
-        </div>
+        <div className="text-center text-red-500">{errors.root.message}</div>
       )}
 
       <>
-        <div className="relative lg:w-3/4 sm:max-md:w-[60%] w-[60%] flex justify-center items-center">
+        <div className="relative flex w-[60%] items-center justify-center sm:max-md:w-[60%] lg:w-3/4">
           <div className="flex-grow border-t border-black"></div>
           <span className="flex-shrink p-2">or</span>
           <div className="flex-grow border-t border-black"></div>
         </div>
 
-        <Button 
+        <Button
           className="flex space-x-2"
           variant="outline"
           disabled={status}
           onClick={navigateToGoogleRedirectURI}
         >
-          <Icon 
-            src='/icons/google.svg'
-            alt='google-icon'
-            height='20px'
-            width='20px'
+          <Icon
+            src="/icons/google.svg"
+            alt="google-icon"
+            height="20px"
+            width="20px"
           />
           <p>Continue with Google</p>
         </Button>
@@ -142,16 +127,16 @@ function LoginForm({ className, onClick }: LoginFormProps) {
         <div>
           <span>Don&apos;t have an account?</span>
           <Button
-            variant="link" 
-            className="text-blue-600 p-1.5 cursor-pointer underline"
+            variant="link"
+            className="cursor-pointer p-1.5 text-blue-600 underline"
             onClick={onClick}
           >
             Sign up
           </Button>
-        </div> 
+        </div>
       </>
-    </div> 
+    </div>
   );
 }
 
-export default memo(LoginForm);
+export default LoginForm;
