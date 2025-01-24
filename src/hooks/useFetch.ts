c4a70c0f1917c3b3d6610a8f7562d4023816309a
Mohan Drey <mohantampon@gmail.com>
2025-01-24 17:41:51 +0800

    format useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,8 +1,11 @@
 const useFetch = async <T>(endpoint: string, method?: string): Promise<T> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${endpoint}`, {
-    credentials: 'include',
-    method,
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}${endpoint}`,
+    {
+      credentials: "include",
+      method,
+    },
+  );
 
   if (!response.ok) {
     throw new Error(`Error fetching data: ${response.statusText}`);
