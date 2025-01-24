0fb4a31a040052750675d6374d188f7807df234a
Mohan Drey <mohantampon@gmail.com>
2025-01-24 17:42:30 +0800

    update h dropdown component

--- src/components/HDropDown.tsx
+++ src/components/HDropDown.tsx
@@ -1,9 +1,9 @@
-import { 
+import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuTrigger
+  DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { Avatar, AvatarFallback } from "./ui/avatar";
 import type { User } from "@/app/dashboard/types";
@@ -17,38 +17,37 @@ interface Props {
   onLogout?: () => void;
 }
 
-export default function HDropdown ({ userState, label, items, onLogout }: Props) {
+export default function HDropdown({
+  userState,
+  label,
+  items,
+  onLogout,
+}: Props) {
   const router = useRouter();
 
   const handleDropDown = (item: string): void => {
-    if (item.toLowerCase() === 'logout' && onLogout) {
+    if (item.toLowerCase() === "logout" && onLogout) {
       onLogout();
-    } else if (item.toLowerCase() === 'profile') {
-      router.push('/profile')
+    } else if (item.toLowerCase() === "profile") {
+      router.push("/profile");
     }
-  }
+  };
 
-  return(
+  return (
     <>
       <DropdownMenu>
         <DropdownMenuTrigger>
           <Avatar>
-            <AvatarFallback
-              key={userState?.id}
-              className="text-white"
-            >
+            <AvatarFallback key={userState?.id} className="text-white">
               {getInitials(userState)}
             </AvatarFallback>
           </Avatar>
         </DropdownMenuTrigger>
-        
+
         <DropdownMenuContent className="mr-8 flex flex-col">
           <DropdownMenuLabel>{label}</DropdownMenuLabel>
-          {items.map(item => (
-            <DropdownMenuItem
-              key={item}
-              onClick={() => handleDropDown(item)}
-            >
+          {items.map((item) => (
+            <DropdownMenuItem key={item} onClick={() => handleDropDown(item)}>
               {item}
             </DropdownMenuItem>
           ))}
@@ -56,4 +55,4 @@ export default function HDropdown ({ userState, label, items, onLogout }: Props)
       </DropdownMenu>
     </>
   );
-};
+}
