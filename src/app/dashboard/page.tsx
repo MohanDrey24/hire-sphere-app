1ca6c477e3c40823b4a0c7f29a0707036f7656f2
Mohan Drey <mohantampon@gmail.com>
2025-01-25 17:24:03 +0800

    change formatting

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -13,27 +13,17 @@ export default function Dashboard() {
   const jobState = useJobStore((state) => state.jobs);
 
   return (
-    <div className="grid grid-cols-8 grid-rows-8 h-screen w-screen relative">
+    <div className="relative grid h-screen w-screen grid-cols-8 grid-rows-8">
       <NavBar className="col-span-8" />
 
       <JobCard
-        className={`
-          col-span-8 row-span-7 row-start-2
-          overflow-y-auto
-          ${selectedJobId ? "hidden" : "block"}
-          sm:block sm:col-span-3
-        `}
+        className={`col-span-8 row-span-7 row-start-2 overflow-y-auto ${selectedJobId ? "hidden" : "block"} sm:col-span-3 sm:block`}
         jobData={jobState}
         selectedJobId={selectedJobId}
       />
 
       <JobDetails
-        className={`
-          col-span-8 row-span-7 row-start-2
-          ${selectedJobId ? "block" : "hidden"}
-          sm:block sm:col-span-5 sm:col-start-4 sm:row-start-2
-          bg-white
-        `}
+        className={`col-span-8 row-span-7 row-start-2 ${selectedJobId ? "block" : "hidden"} bg-white sm:col-span-5 sm:col-start-4 sm:row-start-2 sm:block`}
         selectedJobId={selectedJobId}
       />
     </div>
