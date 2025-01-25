d936a0b5c7ef945b5f34956cae6b5ec3f1533e26
Mohan Drey <mohantampon@gmail.com>
2025-01-25 17:24:19 +0800

    remove set user

--- src/app/stores/useUserStore.ts
+++ src/app/stores/useUserStore.ts
@@ -1,35 +1,14 @@
-import { create } from 'zustand';
-import { type User } from '../dashboard/types';
-import useFetch from '@/hooks/useFetch';
-import { useQuery } from '@tanstack/react-query';
+import { create } from "zustand";
+import { type User } from "../dashboard/types";
 
 type UserState = {
   user: User | null;
   isLoading: boolean;
-}
+};
 
-type UserAction = {
-  setUser: () => void
-}
-
-const useUserStore = create<UserState & UserAction>((set) => ({
+const useUserStore = create<UserState>((set) => ({
   user: null,
   isLoading: false,
-  setUser: () => {
-    const { data, isPending: isLoading } = useQuery<User>({
-      queryKey: ['users'],
-      queryFn: () => useFetch('/users/current'),
-      staleTime: 1000 * 60 * 5,
-    })
-
-    if (data) {
-      set({ user: data })
-    }
-
-    if (isLoading) {
-      set({ isLoading })
-    }
-  }
-}))
+}));
 
 export default useUserStore;
