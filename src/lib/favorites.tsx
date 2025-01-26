f6a0bc29e187dd383aa74975762248c92cf16ea3
Mohan Drey <mohantampon@gmail.com>
2025-01-26 14:43:30 +0800

    add favorites api

---
+++ src/lib/favorites.tsx
@@ -0,0 +1,18 @@
+import { Favorites } from "@/app/dashboard/types";
+import { useQuery, queryOptions } from "@tanstack/react-query";
+import { api } from "./api-client";
+
+export const getFavorites = async () => {
+  return api.get<Favorites>("/favorites");
+};
+
+const favoritesQuerykey = ["favorites"];
+
+export const getFavoritesQueryOptions = () => {
+  return queryOptions({
+    queryKey: favoritesQuerykey,
+    queryFn: getFavorites,
+  });
+};
+
+export const useGetFavorites = () => useQuery(getFavoritesQueryOptions());
