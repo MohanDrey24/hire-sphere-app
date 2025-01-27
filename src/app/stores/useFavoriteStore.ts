793cf7830e0f90decfe89334c23ed8cce8dc0ca7
Mohan Drey <mohantampon@gmail.com>
2025-01-27 18:05:12 +0800

    fix formatting

--- src/app/stores/useFavoriteStore.ts
+++ src/app/stores/useFavoriteStore.ts
@@ -6,30 +6,30 @@ import { useQuery } from "@tanstack/react-query";
 type State = {
   favorites: Favorites[];
   isLoading: boolean;
-}
+};
 
 type Action = {
   fetchFavorites: () => void;
-}
+};
 
 const useFavoriteStore = create<State & Action>((set) => ({
   favorites: [],
   isLoading: false,
   fetchFavorites: () => {
-    const {data, isPending} = useQuery<Favorites[]>({
+    const { data, isPending } = useQuery<Favorites[]>({
       queryKey: ["favorites"],
-      queryFn: () =>  useFetch("/favorites"),
-      staleTime: 1000 * 60 * 5
-    })
+      queryFn: () => useFetch("/favorites"),
+      staleTime: 1000 * 60 * 5,
+    });
 
     if (data) {
-      set({ favorites: data })
+      set({ favorites: data });
     }
 
     if (isPending) {
-      set({ isLoading: isPending })
+      set({ isLoading: isPending });
     }
   },
-}))
+}));
 
 export default useFavoriteStore;
