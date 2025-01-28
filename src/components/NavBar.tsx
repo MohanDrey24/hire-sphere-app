d4474d8b93ad36be36e6082633db7be366db73e8
Mohan Drey <mohantampon@gmail.com>
2025-01-28 17:31:43 +0800

    fix formatting

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { memo } from "react";
 import { MouseEventHandler } from "react";
 import { Button } from "./ui/button";
@@ -8,20 +10,15 @@ interface NavBarProps {
   onClickSignUp?: MouseEventHandler<HTMLButtonElement>;
 }
 
-function NavBar ({ className, onClickLogin, onClickSignUp }: NavBarProps) {
+function NavBar({ className, onClickLogin, onClickSignUp }: NavBarProps) {
   return (
-    <nav className={`flex items-center border-gray border-b-2 ${className}`}>
-      <div className="border-none ml-10 bg-red-100 w-10 h-10 rounded-full" />
-      <div className="space-x-4 ml-auto px-12 *:w-20 *:h-8">
-        <Button 
-          variant="outline"
-          onClick={onClickLogin}
-        >
+    <nav className={`border-gray flex items-center border-b-2 ${className}`}>
+      <div className="ml-10 h-10 w-10 rounded-full border-none bg-red-100" />
+      <div className="ml-auto space-x-4 px-12 *:h-8 *:w-20">
+        <Button variant="outline" onClick={onClickLogin}>
           Log in
         </Button>
-        <Button onClick={onClickSignUp}>
-          Sign Up
-        </Button>
+        <Button onClick={onClickSignUp}>Sign Up</Button>
       </div>
     </nav>
   );
