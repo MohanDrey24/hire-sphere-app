a41a7b5672bfc103224430d64bf9023ff0c544fc
Mohan Drey <mohantampon@gmail.com>
2025-01-28 17:31:22 +0800

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,23 +3,23 @@ import { NextRequest } from "next/server";
 import { cookies } from "next/headers";
 import { decryptJWT } from "./app/auth/jwt";
 
-const protectedRoutes = ['/dashboard'];
-const publicRoutes = ['/'];
+const protectedRoutes = ["/dashboard"];
+const publicRoutes = ["/"];
 
 export async function middleware(request: NextRequest) {
-  const session = cookies().get('HS')?.value;
+  const session = cookies().get("HS")?.value;
 
   const path = request.nextUrl.pathname;
   const isProtectedRoute = protectedRoutes.includes(path);
   const isPublicRoute = publicRoutes.includes(path);
-  const isLoggedIn = await decryptJWT(session)
+  const isLoggedIn = await decryptJWT(session);
 
   /**
-   * if there is no session and they try to access a 
+   * if there is no session and they try to access a
    * protected route redirect the user back to index page
    */
   if (!session && isProtectedRoute) {
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   /**
@@ -28,9 +28,9 @@ export async function middleware(request: NextRequest) {
   if (
     isPublicRoute &&
     isLoggedIn?.id &&
-    !request.nextUrl.pathname.startsWith('/dashboard')
+    !request.nextUrl.pathname.startsWith("/dashboard")
   ) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   // For all other cases, continue with the request
