83d249297d5df6afe616b8e6ff09966e9715a8ad
Mohan Drey <mohantampon@gmail.com>
2025-01-29 16:37:53 +0800

    update navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,37 @@
-"use client";
-
 import { memo } from "react";
 import { MouseEventHandler } from "react";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 interface NavBarProps {
-  className?: string | undefined;
   onClickLogin?: MouseEventHandler<HTMLButtonElement>;
   onClickSignUp?: MouseEventHandler<HTMLButtonElement>;
 }
 
-function NavBar({ className, onClickLogin, onClickSignUp }: NavBarProps) {
+function NavBar({ onClickLogin, onClickSignUp }: NavBarProps) {
   return (
-    <nav className={`border-gray flex items-center border-b-2 ${className}`}>
-      <div className="ml-10 h-10 w-10 rounded-full border-none bg-red-100" />
-      <div className="ml-auto space-x-4 px-12 *:h-8 *:w-20">
-        <Button variant="outline" onClick={onClickLogin}>
-          Log in
+    <nav className="flex h-24 items-center">
+      <Image
+        src="/icons/hire_sphere_logo.svg"
+        width={240}
+        height={240}
+        alt="HS Logo"
+        className="ml-5 hidden sm:block"
+      />
+      <Image
+        src="/icons/sphere.svg"
+        width={70}
+        height={70}
+        alt="Sphere Logo"
+        className="ml-5 block sm:hidden"
+      />
+      <div className="ml-auto flex space-x-4 px-12 *:h-8 *:w-20">
+        <Button variant="fuchsia" onClick={onClickLogin}>
+          <span className="font-bold text-fuchsia-light">Log in</span>
         </Button>
-        <Button onClick={onClickSignUp}>Sign Up</Button>
+        <Button variant="brown" onClick={onClickSignUp}>
+          <span className="font-bold">Sign Up</span>
+        </Button>
       </div>
     </nav>
   );
