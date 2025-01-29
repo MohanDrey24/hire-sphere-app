91408ef2a358e609ebdbead7e0542c99f3f2afd3
Mohan Drey <mohantampon@gmail.com>
2025-01-29 16:37:36 +0800

    update hformfield

--- src/components/forms/HFormField.tsx
+++ src/components/forms/HFormField.tsx
@@ -1,7 +1,13 @@
 import { Control } from "react-hook-form";
-import { FormControl, FormField, FormItem, FormMessage } from "../ui/form";
-import { Input } from "../ui/input";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+  useFormField,
+} from "../ui/form";
 import { MouseEventHandler } from "react";
+import { FloatingLabelInput } from "../ui/floating-label-input";
 
 interface HFormFieldProps {
   control: Control<any>;
@@ -11,35 +17,41 @@ interface HFormFieldProps {
   placeholder: string;
   disabled?: boolean;
   className?: string;
+  required?: boolean;
   onClick?: MouseEventHandler<HTMLInputElement>;
 }
 
-export default function HFormField({ 
-  control, 
-  name, 
-  type = 'text', 
-  placeholder, 
+export default function HFormField({
+  control,
+  name,
+  type = "text",
+  placeholder,
   label,
-  disabled = false, 
+  disabled = false,
   className,
-  onClick, 
+  onClick,
 }: HFormFieldProps) {
+  const { invalid } = useFormField();
+
+  console.log(invalid);
+
   return (
     <FormField
       control={control}
       name={name}
       render={({ field }) => (
         <FormItem>
-          <label>{label}</label>
           <FormControl>
-            <Input
+            <FloatingLabelInput
               {...field}
+              label={label}
               type={type}
               placeholder={placeholder}
               disabled={disabled}
               onClick={onClick}
-              autoComplete="off"
               className={className}
+              required
+              formNoValidate
             />
           </FormControl>
           <FormMessage />
