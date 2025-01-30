f93bfa5877eda3ed5adab0e20dac32a9adbc50f8
Mohan Drey <mohantampon@gmail.com>
2025-01-30 16:25:51 +0800

    update ui in index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { useCallback } from "react";
 import Loginform from "@/components/forms/Loginform";
 import NavBar from "@/components/NavBar";
 import SignupForm from "@/components/forms/SignupForm";
+import { Infinity } from "lucide-react";
 
 const PAGES = {
   LOGIN: "login",
@@ -55,11 +56,41 @@ export default function Home() {
 
       <div className="flex flex-1 flex-row">
         <div className="relative hidden w-0 overflow-hidden sm:hidden lg:block lg:w-1/2 xl:w-1/2">
-          <img
-            className="absolute inset-0 h-full w-full object-cover"
-            src="/images/palette-ball.png"
-            alt="Palette ball"
-          />
+          <div className="flex h-full w-full flex-col justify-center gap-10 p-20">
+            <div className="flex flex-col justify-center">
+              <h2 className="text-fuchsia-dark font-anta text-5xl leading-tight font-bold">
+                Hire Sphere &ndash;
+              </h2>
+              <h3 className="text-5xl leading-snug font-bold">
+                Your Gateway to Top Talent & Dream Jobs!
+              </h3>
+            </div>
+
+            <div className="text-md mx-auto max-w-3xl">
+              Connecting job seekers with their ideal careers and helping
+              employers find the best talent. Fast, easy, and efficient job
+              hosting to power the future of work.
+            </div>
+
+            <div className="flex justify-between">
+              <div className="flex flex-col">
+                <span className="text-mint-green-light text-2xl font-bold">
+                  1000+
+                </span>
+                <span>Jobs</span>
+              </div>
+              <div className="flex flex-col">
+                <span className="text-mint-green-light text-2xl font-bold">
+                  1000+
+                </span>
+                <span>Companies</span>
+              </div>
+              <div className="flex flex-col">
+                <Infinity color="#0be0c0" size={30} />
+                <span>Opportunities</span>
+              </div>
+            </div>
+          </div>
         </div>
 
         {currentPage === PAGES.LOGIN ? (
