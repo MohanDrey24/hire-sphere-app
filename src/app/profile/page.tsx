65be9eee24ce032a657b9fb2444b8707b6489beb
Mohan Drey <mohantampon@gmail.com>
2025-02-15 22:35:36 +0800

    update profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,11 @@
 
 import { useEffect } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 
+import { Avatar } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useCurrentUser } from "@/lib/auth";
@@ -13,6 +15,7 @@ import {
   useUpdateUserProfile,
   type UserFormData,
 } from "@/lib/user";
+import { getInitials } from "../utils/getInitials";
 
 export default function Profile() {
   const queryClient = useQueryClient();
@@ -22,7 +25,7 @@ export default function Profile() {
   const {
     register,
     handleSubmit,
-    setValue,
+    reset,
     formState: { errors },
   } = useForm<UserFormData>({
     resolver: zodResolver(userFormSchema),
@@ -30,11 +33,13 @@ export default function Profile() {
 
   useEffect(() => {
     if (userState) {
-      setValue("name", userState.name);
-      setValue("firstName", userState.firstName);
-      setValue("lastName", userState.lastName);
+      reset({
+        name: userState.name || "",
+        firstName: userState.firstName || "",
+        lastName: userState.lastName || "",
+      });
     }
-  }, [userState, setValue]);
+  }, [userState, reset]);
 
   const onSubmit = (data: UserFormData) => {
     mutate(data, {
@@ -48,11 +53,32 @@ export default function Profile() {
   };
 
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-10 p-5 sm:p-20">
+      <p className="items-center text-4xl">PROFILE</p>
+
+      <div>
+        <Avatar className="h-40 w-40 bg-amber-600 flex items-center justify-center">
+          <AvatarImage alt="Avatar" src={userState?.image} />
+          <AvatarFallback className="text-6xl font-bold text-black">
+            {getInitials(userState ?? null)}
+          </AvatarFallback>
+        </Avatar>
+      </div>
+
       <form
-        className="flex flex-col gap-4 w-1/2"
+        className="flex flex-col gap-4 w-full"
         onSubmit={handleSubmit(onSubmit)}
       >
+        <div>
+          <label>Email:</label>
+          <Input
+            id="email"
+            name="email"
+            disabled
+            value={userState?.email || ""}
+          />
+        </div>
+
         <div>
           <label>Name:</label>
           <Input
@@ -97,104 +123,3 @@ export default function Profile() {
     </div>
   );
 }
-
-// "use client";
-
-// import { useState } from "react";
-// import { SquareXIcon, UserPenIcon } from "lucide-react";
-
-// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-// import { Button } from "@/components/ui/button";
-// import { useCurrentUser } from "@/lib/auth";
-// import { formatDate } from "../utils/formatDate";
-// import { getInitials } from "../utils/getInitials";
-// import ProfileField from "./ProfileField";
-
-// export default function Profile() {
-//   const [isEditing, setEditingMode] = useState(false);
-//   const { data: userState } = useCurrentUser();
-
-//   return (
-//     <div className="flex h-screen w-screen flex-col items-center justify-center gap-10 p-5 sm:p-20">
-//       <p className="items-center text-4xl">PROFILE</p>
-//       <div className="flex w-3/4 flex-col items-center gap-10 sm:w-3/4 md:flex-row">
-//         <Avatar className="h-40 w-40">
-//           <AvatarImage alt="Avatar" src={userState?.image} />
-//           <AvatarFallback className="text-5xl font-bold text-white">
-//             {getInitials(userState ?? null)}
-//           </AvatarFallback>
-//         </Avatar>
-
-//         <div className="flex w-full flex-col gap-2">
-//           {!isEditing && (
-//             <div className="self-end">
-//               <Button
-//                 variant="ghost"
-//                 size="icon"
-//                 aria-label="Edit Profile"
-//                 onClick={() => setEditingMode(!isEditing)}
-//               >
-//                 <UserPenIcon />
-//               </Button>
-//             </div>
-//           )}
-
-//           <ProfileField
-//             className="pb-4"
-//             label="Display Name"
-//             name="Display Name"
-//             isEditing={isEditing}
-//             value={userState?.name}
-//           />
-
-//           <div className="flex flex-col gap-4 sm:flex-row">
-//             <ProfileField
-//               className="w-full sm:w-1/2"
-//               label="First Name"
-//               name="First Name"
-//               isEditing={isEditing}
-//               value={userState?.firstName}
-//             />
-
-//             <ProfileField
-//               className="w-full sm:w-1/2"
-//               label="Last Name"
-//               name="Last Name"
-//               isEditing={isEditing}
-//               value={userState?.lastName}
-//             />
-//           </div>
-//         </div>
-//       </div>
-
-//       <div>
-//         <ul className="flex flex-col space-y-2 sm:space-y-0 md:flex-row">
-//           <li className="flex items-center gap-2">
-//             <span className="text-gray-700">Email:</span>
-//             <p className="font-bold">{userState?.email}</p>
-//           </li>
-//           <li className="flex items-center gap-2">
-//             <span className="text-gray-700">Joined at:</span>
-//             <p className="font-bold">
-//               {formatDate(userState?.createdAt, "MM/DD/YYYY")}
-//             </p>
-//           </li>
-//         </ul>
-//       </div>
-
-//       {isEditing && (
-//         <div className="flex gap-4">
-//           <Button type="submit">Save Changes</Button>
-//           <Button
-//             variant="ghost"
-//             size="icon"
-//             type="button"
-//             onClick={() => setEditingMode(false)}
-//           >
-//             <SquareXIcon />
-//           </Button>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
